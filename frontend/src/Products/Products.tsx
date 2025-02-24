b64b439d93b7aa2e46397a591175c8f3dcd49181
Ed Yang <yang.5885@buckeyemail.osu.edu>
2025-02-23 20:18:49 -0500

    lab3

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,8 +1,28 @@
 function Products() {
     return (
-        <div>
-            <h2>Products</h2>
-        </div>
-    );
+            <div className="content">
+                <ul className="products">
+                    {data.products.map((product) (
+                        <li>
+                            <div className="product">
+                                <img
+                                className="product-image"
+                                src={Products.imageUrl}
+                                alt="product"
+                                />
+                                <div className="product-name">
+                                    <a href="product.html">{product.name}</a>
+                                    </div>
+                                    <div className="product-brand">{Products.brand}</div>
+                                    <div className="product-price">{Products.price}</div>
+                                    <div className="product-rating">
+                                        {Products.rating} Stars ({Products.numberOfReviews} reviews)
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+    )
 }
 export default Products;
---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,44 @@
+import data from "./data";
+export default{
+    products: [{
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageURL: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageURL: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageURL: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageURL: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    
+    }]
+}
