d39c605c4068374e12a267f1ec808f4d92cabb43
Ed Yang <yang.5885@buckeyemail.osu.edu>
2025-02-23 20:30:23 -0500

    lab3

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,22 +1,24 @@
+import data from "./data";
+
 function Products() {
     return (
             <div className="content">
                 <ul className="products">
-                    {data.products.map((product) (
+                    {data.products.map((product) => (
                         <li>
                             <div className="product">
                                 <img
                                 className="product-image"
-                                src={Products.imageUrl}
+                                src={product.imageUrl}
                                 alt="product"
                                 />
                                 <div className="product-name">
                                     <a href="product.html">{product.name}</a>
                                     </div>
-                                    <div className="product-brand">{Products.brand}</div>
-                                    <div className="product-price">{Products.price}</div>
+                                    <div className="product-brand">{product.brand}</div>
+                                    <div className="product-price">{product.price}</div>
                                     <div className="product-rating">
-                                        {Products.rating} Stars ({Products.numberOfReviews} reviews)
+                                        {product.rating} Stars ({product.numberOfReviews} reviews)
                                 </div>
                             </div>
                         </li>
